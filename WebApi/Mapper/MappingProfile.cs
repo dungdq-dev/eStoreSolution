c00f39d8305cafe3b68750d4a17cd67c2c646a5c
Dau Dung <qdau79@gmail.com>
2024-10-31 11:47:39 +0700

    add mapping profile

---
+++ WebApi/Mapper/MappingProfile.cs
@@ -0,0 +1,23 @@
+﻿using AutoMapper;
+using DataAccess.Entities;
+using ViewModels.Catalog.Categories;
+using ViewModels.System.Roles;
+using ViewModels.System.Users;
+
+namespace WebApi.Mapper
+{
+    public class MappingProfile : Profile
+    {
+        public MappingProfile()
+        {
+            CreateMap<UserDto, AppUser>();
+            CreateMap<AppUser, UserDto>();
+
+            CreateMap<RoleDto, AppRole>();
+            CreateMap<AppRole, RoleDto>();
+
+            CreateMap<CategoryDto, Category>();
+            CreateMap<Category, CategoryDto>();
+        }
+    }
+}
